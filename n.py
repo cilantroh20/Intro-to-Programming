c9f747ac112772613cee47cfc511333094e3e743
cilantroh20 <celesteduncan21@yahoo.com>
2016-08-30 17:39:35 -0700

    Update n.py

--- n.py
+++ n.py
@@ -1,48 +1,36 @@
-# replaced with a random verb. You're free to change what the random functions
-# return as verbs or nouns for your own fun, but for submissions keep the code the way it is!
+# IPND Stage 2 Final Project
 
-from random import randint
+# You've built a Mad-Libs game with some help from Sean.
+# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
 
-def random_verb():
-    random_num = randint(0, 1)
-    if random_num == 0:
-        return "run"
-    else:
-        return "kayak"
-        
-def random_noun():
-    random_num = randint(0,1)
-    if random_num == 0:
-        return "sofa"
-    else:
-        return "llama"
+# For this project, you'll be building a Fill-in-the-Blanks quiz.
+# Your quiz will prompt a user with a paragraph containing several blanks.
+# The user should then be asked to fill in each blank appropriately to complete the paragraph.
+# This can be used as a study tool to help you remember important vocabulary!
 
-def word_transformer(word):
-    if word == "NOUN":
-        return random_noun()
-    elif word == "VERB":
-        return random_verb()
-    else:
-        return word[0]
-        
-def process_madlib(mad_lib):
-    processed = ""
-    index = 0
-    box_length = 4
-    while index < len (madlib):
-        frame = madlib[index: index + box_length]
-        to_add = word_transformer(frame)
-        processed += to_add
-        if len (to_add) == 1:
-            index += 2
-        else:
-            index += 4 
-            return processed 
-    # your code here
-    # you may find the built-in len function useful for this quiz
-    # documentation: https://docs.python.org/2/library/functions.html#len
-    
-test_string_1 = "This is a good NOUN to use when you VERB your food"
-test_string_2 = "I'm going to VERB to the store and pick up a NOUN or two."
-print process_madlib(test_string_1)
-print process_madlib(test_string_2)
+# Note: Your game will have to accept user input so, like the Mad Libs generator,
+# you won't be able to run it using Sublime's `Build` feature.
+# Instead you'll need to run the program in Terminal or IDLE.
+# Refer to Work Session 5 if you need a refresher on how to do this.
+
+# To help you get started, we've provided a sample paragraph that you can use when testing your code.
+# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
+
+sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+
+# The answer for ___1___ is 'function'. Can you figure out the others?
+
+# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
+# A .pyc file is a Python file that has been translated into "byte code".
+# This means the code will run the same as the original .py file, but when you open it
+# it won't look like Python code! But you can run it just like a regular Python file
+# to see how your code should behave.
+
+# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
+# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
+# How can you adapt that design to work with numbered blanks?
+
+# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd1-1/20min/
